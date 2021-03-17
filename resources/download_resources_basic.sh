f36f4a2d9e85f3b62f51909ef426be984990682c
Indrakumar Mhaski <indrakumarmhaski@gmail.com>
2021-03-17 18:22:54 +0530

    first commit

--- resources/download_resources_basic.sh
+++ resources/download_resources_basic.sh
@@ -3,3 +3,4 @@ git clone https://github.com/zllrunning/face-parsing.PyTorch.git face_parsing_py
 gdown https://drive.google.com/uc?id=154JgKpzCPW82qINcVieuPH3fZ2e0P812
 mkdir -p face_parsing_pytorch/res/cp
 mv 79999_iter.pth face_parsing_pytorch/res/cp
+# 
